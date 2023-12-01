e283f850c4ccc951189289496504679211b56f2b
mhoarb <mho.arby@gmail.com>
2023-12-01 04:22:01 +0330

    add Difficulity to block

--- Blockchain_Struct.js
+++ Blockchain_Struct.js
@@ -1,16 +1,32 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-  constructor(index, timestamp, data, previousHash = '') {
+  constructor(index, timestamp, data, previousHash = '', difficulty = 5) {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
+    this.difficulty = difficulty; 
+    this.nonce = 0;
     this.hash = this.calculateHash();
   }
 
   calculateHash() {
-    return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+    return SHA256(
+      this.index +
+      this.timestamp +
+      JSON.stringify(this.data) +
+      this.previousHash +
+      this.nonce
+    ).toString();
+  }
+
+  mineBlock() {
+    while (this.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join('0')) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log(`Block mined: ${this.hash}`);
   }
 }
 
@@ -29,7 +45,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(); // Mine the block before adding to the chain
     this.chain.push(newBlock);
   }
 
@@ -52,4 +68,3 @@ class Blockchain {
 }
 
 module.exports = { Blockchain, Block };
-
